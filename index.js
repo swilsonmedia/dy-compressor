971f73ce49d3f96314af101db01dea0a97554e8b
Stephen Wilson <info@swilsonmedia.com>
2020-01-30 10:11:39 -0500

    First commit.  Added POST endpoints for minify and beautify

---
+++ index.js
@@ -0,0 +1,106 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const app = express();
+
+const htmlMinify = require('html-minifier').minify;
+const beautify = require('beautify');
+const pretty = require('pretty');
+const uglifyJS = require('uglify-js').minify;
+const uglifyCSS = require('uglifycss').processString;
+
+const port = process.env.PORT || 5000;
+
+const JS = 'js';
+const CSS = 'css';
+const HTML = 'html';
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('*', function(req, res) {
+	res.send('Page not found');
+});
+
+app.post('/minify', function(req, res) {
+	if (!req.body.type || !req.body.data) {
+		return res.json({
+			status: 'error',
+			data: 'A property was missing'
+		});
+	}
+
+	if (req.body.type.toLowerCase() === HTML) {
+		return res.json({
+			status: 'ok',
+			data: htmlMinify(req.body.data, {
+				collapseWhitespace: true,
+				removeComments: true,
+				removeTagWhitespace: true,
+				minifyCss: true,
+				minifyJs: true
+			})
+		});
+	}
+
+	if (req.body.type.toLowerCase() === JS) {
+		return res.json({
+			status: 'ok',
+			data: uglifyJS(req.body.data, {
+				mangle: false,
+				compress: {
+					expression: true
+				}
+			})
+		});
+	}
+
+	if (req.body.type.toLowerCase() === CSS) {
+		return res.json({
+			status: 'ok',
+			data: uglifyCSS(req.body.data)
+		});
+	}
+
+	res.json({
+		status: 'error',
+		data: '"type" property not recognized'
+	});
+});
+
+app.post('/beautify', function(req, res) {
+	if (!req.body.type || !req.body.data) {
+		return res.json({
+			status: 'error',
+			data: 'A property was missing'
+		});
+	}
+
+	if (req.body.type.toLowerCase() === JS) {
+		return res.json({
+			status: 'ok',
+			data: beautify(req.body.data, {format: JS})
+		});
+	}
+
+	if (req.body.type.toLowerCase() === HTML) {
+		return res.json({
+			status: 'ok',
+			data: pretty(req.body.data)
+		});
+	}
+
+	if (req.body.type.toLowerCase() === CSS) {
+		return res.json({
+			status: 'ok',
+			data: beautify(req.body.data, {format: CSS})
+		});
+	}
+
+	res.json({
+		status: 'error',
+		data: '"type" property not recognized'
+	});
+});
+
+app.listen(port, () => console.log(`Listen to port ${port}`));
